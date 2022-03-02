c37a7bc6203652aa0a75b1c3bb6ad9e9f662cebe
DESKTOP-MTGVP5G\DELL <4146kt@gmail.com>
2022-03-02 11:19:27 +0900

    New Project Ex03

---
+++ ex03/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,95 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+		/*getList() 결과 값이 하나도 없을 때까지, 결과 값을 하나씩 BoardVO 타입의 board로 받는다. 그리고 log.info(board)를 실행한다. */
+	}
+	
+	@Test
+	public void insert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("title");
+		board.setContent("content");
+		board.setWriter("writer");
+	
+		mapper.insert(board);
+		log.info(board);
+	}
+
+	@Test
+	public void insertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("title");
+		board.setContent("content");
+		board.setWriter("writer");
+	
+		mapper.insertSelectKey(board);
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}
+
+	@Test
+	public void testDelete() {
+		log.info(mapper.delete(5L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(8L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("Update Count: " + count);
+	}
+	
+	@Test
+	public void testPaging() {
+		Criteria cri = new Criteria();
+		
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testSearch() {
+		Criteria cri = new Criteria();
+		cri.setKeyword("");
+		cri.setType("");
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
+	}
+	
+}
