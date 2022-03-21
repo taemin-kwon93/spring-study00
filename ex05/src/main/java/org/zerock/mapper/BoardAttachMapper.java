82b19c99cfb28608ec5da0183dc4c50813ed0eb5
DESKTOP-MTGVP5G\DELL <4146kt@gmail.com>
2022-03-21 15:56:19 +0900

    Spring Quartz 적용
    Quartz를 이용해서,
    DB에 있는 값과 폴더에 있는 파일을 비교합니다.
    비교 후 DB에 존재하지 않고 폴더에만 존재하는 파일을 삭제해줍니다.
    
    예를들어
    사진이나 pdf파일이 업로드 된 후 폴더에 값이 저장됐고

--- ex05/src/main/java/org/zerock/mapper/BoardAttachMapper.java
+++ ex05/src/main/java/org/zerock/mapper/BoardAttachMapper.java
@@ -14,4 +14,5 @@ public interface BoardAttachMapper {
 	
 	public void deleteAll(Long bno);
 	
+	public List<BoardAttachVO> getOldFiles();
 }
--- ex05/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex05/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -66,7 +66,7 @@ public class BoardServiceImpl implements BoardService{
 				attachMapper.insert(attach);
 			});
 		}
-
+		
 		return modifyResult;
 	}
 
